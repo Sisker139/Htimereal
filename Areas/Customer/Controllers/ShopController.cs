acd3cbb34f4e6da3645b84ae6bd05a3ebdb049ac
Sisker139 <thi15062@gmail.com>
2025-05-01 13:48:48 +0700

    done

--- Areas/Customer/Controllers/ShopController.cs
+++ Areas/Customer/Controllers/ShopController.cs
@@ -1,5 +1,7 @@
 ﻿using Htime.Data;
 using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
+using X.PagedList.Extensions;
 
 namespace Htime.Areas.Customer.Controllers
 {
@@ -11,11 +13,31 @@ namespace Htime.Areas.Customer.Controllers
         {
             db = context;
         }
-        public IActionResult Index()
+        public IActionResult Index(string? brand, int page = 1)
         {
-            var products = db.Products.Where(p => p.IsActive).ToList();
-            return View(products);
+            int pageSize = 9;
+            var productsQuery = db.Products.Where(p => p.IsActive);
+
+            if (!string.IsNullOrEmpty(brand))
+            {
+                productsQuery = productsQuery.Where(p => p.Brand == brand);
+            }
+
+            var pagedProducts = productsQuery.OrderBy(p => p.Id).ToPagedList(page, pageSize);
+
+            var brandCounts = db.Products
+                .Where(p => p.IsActive)
+                .GroupBy(p => p.Brand)
+                .Select(g => new { Brand = g.Key, Count = g.Count() })
+                .ToList();
+
+            ViewBag.BrandCounts = brandCounts;
+            ViewBag.SelectedBrand = brand;
+
+            return View(pagedProducts);
         }
+
+
 
 
         public IActionResult Detail(int id)
